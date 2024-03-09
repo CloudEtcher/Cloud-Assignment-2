a1269c1d5922fca7c17c1faf7895755155ec5991
CloudEtcher <ojugbeleniola@gmail.com>
2024-03-09 18:25:40 +0100

    Update Disk_usage.sh

--- Disk_usage.sh
+++ Disk_usage.sh
@@ -1,39 +1,46 @@
 #!/bin/bash
 
-directory=$@
+# Default values
+show_all=false
+top_entries=8
 
-number_of_files=$(ls -l "$directory" | grep -c "^")
+while getopts ":dna:" opt; do
+  case $opt in
+    d)
+      show_all=true
+      ;;
+    n)
+      top_entries=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
 
-if [[ $1 == '-d' ]]; then
-  dir_files=true
-  shift 1
+shift $((OPTIND - 1))
+directories=("$@")
+
+if [ ${#directories[@]} -eq 0 ]; then
+  echo "Error: Please provide at least one directory to check disk usage."
+  exit 1
 fi
 
-if [[ $1 == '-n' && $2 =~ ^[0-9]+$ ]]; then
-  entries=$2
-  shift 2
-else
-  entries=8
-fi
-
-list_disk_usage() {
-  if [[ "$dir_files" == true ]]; then
-    # for -d flag
-    sudo du -ah "$1" | sort -hr | head -"$entries"
-    echo " "
-    echo "Total number of entries displayed = ${entries[@]}"
+display_disk_usage() {
+  directory=$1
+  if $show_all; then
+    du -ah "$directory" | sort -rh | head -n "$top_entries"
   else
-    # for -n flag
-    sudo du -h --max-depth=1 "$1" | sort -hr | head -"$entries"
-    echo " "
-    echo "Total number of entries displayed = $entries"
+    du -h --max-depth=1 "$directory" | sort -rh | head -n "$top_entries"
   fi
 }
 
-for dir in $directory; do
-  echo "Disk Usage for directory: $dir"
-  list_disk_usage "$dir"
+for dir in "${directories[@]}"; do
+  echo -e "\nDisk usage for $dir:"
+  display_disk_usage "$dir"
 done
-
-echo " "
-echo "The total number of files in '$directory' = $number_of_files"
